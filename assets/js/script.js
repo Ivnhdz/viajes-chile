e875df734f19e529246e967780b0f48b81685259
IHernandezB <ivan.hdzbernales@gmail.com>
2024-07-11 15:07:47 -0400

    navbar lista

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// hago que mi navbar con id navbar tenga un background transparente //
+document.addEventListener("scroll", function () {
+    let navbar = document.getElementById("navbar");
+    navbar.style.backgroundColor = "transparent";
+});
