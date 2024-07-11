f6f59bd20eef84df1bec4f06d8ad9705a23366ac
IHernandezB <ivan.hdzbernales@gmail.com>
2024-07-11 19:00:47 -0400

    se agrega js y se pule imagen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
+
 // hago que mi navbar con id navbar tenga un background transparente //
 document.addEventListener("scroll", function () {
     let navbar = document.getElementById("navbar");
     navbar.style.backgroundColor = "transparent";
 });
+
+$(document).ready(function() {
+    $('a.scrollSuave').on('click', function(event) {
+      let target = this.hash;
+      if (target) {
+        event.preventDefault();
+        let $target = $(target);
+        $('html, body').animate({
+          scrollTop: $target.offset().top
+        }, 800, function() {
+          window.location.hash = target;
+        });
+      }
+    });
+  });
+
+
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
